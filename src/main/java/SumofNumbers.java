22796b0b0e090c33cd4ff8f929945379dc8d4a39
Caleb <Caleb@192.168.1.110>
2019-09-18 00:16:07 -0500

    Finished Commit

---
+++ src/main/java/SumofNumbers.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+
+ /* To change this license header, choose License Headers in Project Properties.
+ /* To change this template file, choose Tools | Templates
+ /* and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Caleb
+ */
+ 
+public class SumofNumbers
+{public static void main(String[] args) {
+		
+		int num;
+		
+		
+		String input1 = JOptionPane.showInputDialog("Enter a positive nonzero integer");
+		
+	
+		num = Integer.parseInt(input1);
+		
+	
+		int sum = 0;
+		
+	
+		while(num >= 1){
+			sum += num;
+			num--;
+		}
+		JOptionPane.showMessageDialog(null, "Sum: " + sum);
+    
+}
+}
